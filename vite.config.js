5f76b27fd640f6683f3013f9785e7c7c38f9d655
RobD01 <medbz2k3@gmail.com>
2023-09-09 20:09:59 -0700

    publish

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const root = resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+  base: "/QUIZ-WHIZ/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+      },
+    },
+  },
+});
